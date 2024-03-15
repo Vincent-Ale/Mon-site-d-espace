376cd0489bcf0abb68710516f4e19549f3fa4bc4
Vincent-Ale <vincent.aleman@laposte.net>
2024-03-15 12:54:03 +0100

    pagination presque fini

---
+++ HTML - CSS - JAVA/planets.js
@@ -0,0 +1,23 @@
+const imagePaths = [
+    "images/iconplanet/sun2.png", 
+    "images/iconplanet/mercury2.png", 
+    "images/iconplanet/venus2.png", 
+    "images/iconplanet/earth2.png", 
+    "images/iconplanet/mars2.png", 
+    "images/iconplanet/jupiter2.png", 
+    "images/iconplanet/saturn2.png", 
+    "images/iconplanet/uranus2.png",
+    "images/iconplanet/neptune2.png" 
+];
+
+const planetNames = [
+    "Soleil",
+    "Mercure",
+    "Vénus",
+    "Terre",
+    "Mars",
+    "Jupiter",
+    "Saturne",
+    "Uranus",
+    "Neptune"
+];
